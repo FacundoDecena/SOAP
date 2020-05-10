df55cdfd775f4a724691c2e9edfc89d5257b1ff9
Facundo Decena <facundo.decena@gmail.com>
2020-05-10 15:55:13 -0300

    Added request to NumberToWords

--- ejercicio7/src/main/java/controllers/CalculadoraController.java
+++ ejercicio7/src/main/java/controllers/CalculadoraController.java
@@ -7,6 +7,7 @@ import spark.Route;
 import spark.template.velocity.VelocityTemplateEngine;
 import util.Path;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 
 public class CalculadoraController {
@@ -19,7 +20,9 @@ public class CalculadoraController {
         HashMap<String, Object> model = new HashMap<>();
         int sumandoA = Integer.parseInt(request.queryParams("sumandoA"));
         int sumandoB = Integer.parseInt(request.queryParams("sumandoB"));
-        model.put("resultado", CController.addition(sumandoA, sumandoB));
+        int resultado = CController.addition(sumandoA, sumandoB);
+        String res = NCController.convert(BigInteger.valueOf(resultado));
+        model.put("resultado", res);
         return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
     };
 
@@ -27,7 +30,9 @@ public class CalculadoraController {
         HashMap<String, Object> model = new HashMap<>();
         int dividendo = Integer.parseInt(request.queryParams("dividendo"));
         int divisor = Integer.parseInt(request.queryParams("divisor"));
-        model.put("resultado", CController.addition(dividendo, divisor));
+        int resultado = CController.division(dividendo, divisor);
+        String res = NCController.convert(BigInteger.valueOf(resultado));
+        model.put("resultado", res);
         return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
     };
 
@@ -35,7 +40,9 @@ public class CalculadoraController {
         HashMap<String, Object> model = new HashMap<>();
         int multiplicando = Integer.parseInt(request.queryParams("multiplicando"));
         int multiplicador = Integer.parseInt(request.queryParams("multiplicador"));
-        model.put("resultado", CController.addition(multiplicando, multiplicador));
+        int resultado = CController.multiply(multiplicando, multiplicador);
+        String res = NCController.convert(BigInteger.valueOf(resultado));
+        model.put("resultado", res);
         return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
     };
 
@@ -43,7 +50,9 @@ public class CalculadoraController {
         HashMap<String, Object> model = new HashMap<>();
         int minuendo = Integer.parseInt(request.queryParams("minuendo"));
         int sustraendo = Integer.parseInt(request.queryParams("sustraendo"));
-        model.put("resultado", CController.addition(minuendo, sustraendo));
+        int resultado = CController.subtraction(minuendo, sustraendo);
+        String res = NCController.convert(BigInteger.valueOf(resultado));
+        model.put("resultado", res);
         return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
     };
 }
--- ejercicio7/src/main/java/controllers/NCController.java
+++ ejercicio7/src/main/java/controllers/NCController.java
@@ -14,13 +14,8 @@ import java.math.BigInteger;
 import java.util.HashMap;
 
 public class NCController {
-    public static Route toWords = (Request request, Response response) -> {
-        HashMap<String, Object> model = new HashMap<>();
-        model.put("response", convert(BigInteger.valueOf(7)));
-        return new VelocityTemplateEngine().render(new ModelAndView(model, Path.Template.LAYOUT));
-    };
 
-    private static String convert(BigInteger number){
+    public static String convert(BigInteger number){
         NumberToWords req = new NumberToWords();
         req.setUbiNum(number);
         return new NumberConversion().getNumberConversionSoap().numberToWords(req.getUbiNum());
