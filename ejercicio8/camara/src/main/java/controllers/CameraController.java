98e639b43ea235cf12198468434f38ba50e9b4d3
Facundo Decena <facundo.decena@gmail.com>
2020-05-12 00:09:01 -0300

    bug fixed

--- ejercicio8/camara/src/main/java/controllers/CameraController.java
+++ ejercicio8/camara/src/main/java/controllers/CameraController.java
@@ -4,6 +4,8 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -47,6 +49,14 @@ public class CameraController {
             byte[] input = img.getBytes("utf-8");
             os.write(input, 0, input.length);
         }
-        // No time for checking answers
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
     }
 }
