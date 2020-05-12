b23db49a63643fba00854a90c2aad8f7ac90440f
Maxi <maxi.101997@gmail.com>
2020-05-12 00:14:18 -0300

    Finally works

--- ejercicio8/Seguridad/src/main/java/controllers/CaraControlador.java
+++ ejercicio8/Seguridad/src/main/java/controllers/CaraControlador.java
@@ -6,10 +6,12 @@ import models.Cara;
 import spark.Route;
 
 import java.util.List;
+import spark.Request;
+import spark.Response;
 
 public class CaraControlador {
     public static Route reconocimiento =
-            (request, response) -> {
+            (Request request, Response response ) -> {
 
                 String json = request.body();
                 Gson gson = new Gson();
@@ -18,8 +20,15 @@ public class CaraControlador {
                 List<Cara> caras = caraDAO.getCara(cara);
 
                 if(!caras.isEmpty()){
-                    //Llamar servicio alerta
-                    System.out.println("Envio alerta");
+                    try { // Call Web Service Operation
+                        com.mycompany.alarma.Alerta service = new com.mycompany.alarma.Alerta();
+                        com.mycompany.alarma.Alarma port = service.getAlarmaPort();
+                        // TODO initialize WS operation arguments here
+                        java.lang.String img = cara.getImage();
+                        port.alertar(img);
+                    } catch (Exception ex) {
+                        // TODO handle custom exceptions here
+                    }
                 }
                 return "200";
             };
